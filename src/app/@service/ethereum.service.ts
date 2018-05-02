6bcfd364327c22691aa9988f754fb90649f386c9
Gus <gustavo@canya.com>
2018-05-02 14:30:38 -0500

    Adds contract ABI and successfully adds document to contract #2nzxr

--- src/app/@service/ethereum.service.ts
+++ src/app/@service/ethereum.service.ts
@@ -5,9 +5,11 @@ import { Observable, Subject } from 'rxjs';
 declare let require: any;
 declare var window: any;
 
+let _ = require('lodash');
 let Web3 = require('web3');
 let ethers = require('ethers');
 let contract = require('truffle-contract');
+const CANSIGN = require('cansign.v1.abi');
 
 @Injectable()
 export class EthereumService {
@@ -18,9 +20,11 @@ export class EthereumService {
 
   networkType: string
 
-  contractAddress: string = '0x0yghji987'
-
   networkURL: string = 'https://ropsten.etherscan.io'
+
+  contractAddress: string = '0x0ae7483c7f3d40a7516553bff8e338841ca37a4c';
+
+  CanSignContract: any
 
 
   onPublishDocument: Subject<any> = new Subject<any>()
@@ -34,6 +38,10 @@ export class EthereumService {
       this.setWeb3Provider();
       this.getNetworkType();
     }
+
+    if (!this.CanSignContract) {
+      this.setContract();
+    }
   }
 
   openPublishDocumentModal(){
@@ -42,8 +50,57 @@ export class EthereumService {
     });
   }
 
-  publishDocument(document){
+  publishDocument(document) {
     console.log(document);
+
+    let txOptions = {
+      from: this.ETHAddress,
+      to: this.contractAddress,
+      gas: 6000000,
+      gasPrice: 21000000000,
+    };
+
+    let hash = document.hash;
+    let expirationDate = 0;
+    let signers = _.flatMap(document.signers, signer => {
+      return signer.ETHAddress;
+    });
+
+    this.CanSignContract.addDocument.estimateGas(
+      hash,
+      expirationDate,
+      signers,
+      txOptions).then(gas => {
+        console.log(gas);
+        txOptions.gas = gas;
+
+        this.CanSignContract.addDocument(
+          hash,
+          expirationDate,
+          signers,
+          txOptions).then(txn => {
+            console.log(txn);
+          }).catch(error => {
+            console.log(error);
+          });
+
+      }).catch(error => {
+        console.log(error);
+
+      });
+  }
+
+  setContract() {
+    console.log(CANSIGN);
+
+    let c = contract({abi: CANSIGN.abi});
+
+    c.setProvider(this.web3.currentProvider);
+
+    return c.at(this.contractAddress).then(instance => {
+      console.log(instance);
+      this.CanSignContract = instance;
+    }).catch(error => console.log(error));
   }
 
   setWeb3Provider() {
